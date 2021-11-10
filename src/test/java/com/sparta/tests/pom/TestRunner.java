86ca51c28c166a200668d4164d33303cb44d76bd
Lewis <lewisthornton@btinternet.com>
2021-11-10 13:56:57 +0000

    Added TestRunner

---
+++ src/test/java/com/sparta/tests/pom/TestRunner.java
@@ -0,0 +1,13 @@
+package com.sparta.tests.pom;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/features"},
+        plugin = {"pretty", "html:target/testReport.html", "json:target/jsonReport.json"}
+)
+public class TestRunner {
+}
