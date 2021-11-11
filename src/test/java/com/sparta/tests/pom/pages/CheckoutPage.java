35afcbc135d8325a5f1ef8c9fa29130135a97363
Lewis <lewisthornton@btinternet.com>
2021-11-11 12:04:33 +0000

    Added some more helper methods for CheckoutPage

--- src/test/java/com/sparta/tests/pom/pages/CheckoutPage.java
+++ src/test/java/com/sparta/tests/pom/pages/CheckoutPage.java
@@ -2,6 +2,9 @@ package com.sparta.tests.pom.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class CheckoutPage {
     private final WebDriver webDriver;
@@ -14,6 +17,13 @@ public class CheckoutPage {
 
     // Cart button
     By cartButton = new By.ById("shopping_cart_container");
+
+    // Error message
+    By errorMessage = new By.ByCssSelector("[data-test=\"error\"]");
+    By errorMessageExit = new By.ByClassName("error-button");
+
+    // Error symbols
+    By checkoutInfo = new By.ByClassName("checkout_info");
 
     // User input fields
     By firstNameField = new By.ById("first-name");
@@ -32,6 +42,15 @@ public class CheckoutPage {
     // Cart button click
     public void clickCartButton() {
         webDriver.findElement(cartButton).click();
+    }
+
+    // Error messages
+    public String getErrorMessage() {
+        return webDriver.findElement(errorMessage).getText();
+    }
+
+    public void clickErrorMessageExitButton() {
+        webDriver.findElement(errorMessageExit).click();
     }
 
     // Get user input fields
@@ -59,5 +78,22 @@ public class CheckoutPage {
 
     public void setPostCode(String pCode) {
         webDriver.findElement(postalCodeField).sendKeys(pCode);
+    }
+
+    // User input error icons
+    public List<WebElement> getFormGroupItems() {
+        return webDriver.findElement(checkoutInfo).findElements(new By.ByClassName("form_group"));
+    }
+
+    public boolean firstNameHasErrorIcon() {
+        return getFormGroupItems().get(0).findElements(new By.ByTagName("svg")).size() != 0;
+    }
+
+    public boolean lastNameHasErrorIcon() {
+        return getFormGroupItems().get(1).findElements(new By.ByTagName("svg")).size() != 0;
+    }
+
+    public boolean postCodeHasErrorIcon() {
+        return getFormGroupItems().get(2).findElements(new By.ByTagName("svg")).size() != 0;
     }
 }
