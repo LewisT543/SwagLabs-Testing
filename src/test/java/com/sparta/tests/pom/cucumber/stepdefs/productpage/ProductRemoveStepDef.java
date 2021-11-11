bca43a2418e161c03a8f719a016e4f90f30c5443
Professional-Miles <mileslowe@pm.me>
2021-11-11 17:00:33 +0000

    Added a feature and stepdef for decrementing a full cart.

---
+++ src/test/java/com/sparta/tests/pom/cucumber/stepdefs/productpage/ProductRemoveStepDef.java
@@ -0,0 +1,54 @@
+package com.sparta.tests.pom.cucumber.stepdefs.productpage;
+
+import com.sparta.tests.pom.pages.ProductsPage;
+import com.sparta.tests.pom.util.POMUtil;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Random;
+
+public class ProductRemoveStepDef {
+
+    private WebDriver webDriver;
+    private ProductsPage productsPage;
+
+    @Before(order = 1)
+    public void init() {
+        webDriver = POMUtil.getWebDriver();
+        productsPage = new ProductsPage(webDriver);
+    }
+
+    @Given("The cart is full")
+    public void theCartIsFull() {
+        productsPage.clickAddBackpackToCart();
+        productsPage.clickAddBoltTShirtToCartButton();
+        productsPage.clickAddOnesieToCartButton();
+        productsPage.clickAddBikeLightToCartButton();
+        productsPage.clickAddFleeceJacketToCartButton();
+        productsPage.clickAddRedTShirtToCartButton();
+    }
+
+    @When("I click a REMOVE button")
+    public void iClickAREMOVEButton() {
+        Random rand = new Random();
+        int n = rand.nextInt(5) + 1;
+        switch (n) {
+            case 1 -> productsPage.clickRemoveBackpackFromCart();
+            case 2 -> productsPage.clickRemoveBoltTShirtFromCart();
+            case 3 -> productsPage.clickRemoveOnesieFromCart();
+            case 4 -> productsPage.clickRemoveBikeLightFromCart();
+            case 5 -> productsPage.clickRemoveFleeceJacketFromCart();
+            case 6 -> productsPage.clickRemoveRedTShirtFromCart();
+        }
+    }
+
+    @Then("The cart badge should be five")
+    public void theCartBadgeShouldBeFive() {
+        Assertions.assertEquals(5,productsPage.getCartBadgeNumber());
+    }
+
+}
