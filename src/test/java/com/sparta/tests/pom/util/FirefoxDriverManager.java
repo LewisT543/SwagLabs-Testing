6903cad72cb0dcdb6b11dee75b8e36b8a759f885
Gareth <GMitchell@Spartaglobal.com>
2021-11-12 13:41:19 +0000

    refactored driver manager options

--- src/test/java/com/sparta/tests/pom/util/FirefoxDriverManager.java
+++ src/test/java/com/sparta/tests/pom/util/FirefoxDriverManager.java
@@ -42,14 +42,12 @@ public class FirefoxDriverManager extends DriverManager{
         driver = new FirefoxDriver(service, options);
     }
 
-    @Override
-    public DriverManager setHeadless() {
+    public FirefoxDriverManager setHeadless() {
         options.addArguments("headless");
         return this;
     }
 
-    @Override
-    public DriverManager silentOutput() {
+    public FirefoxDriverManager silentOutput() {
         System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/null");
         return this;
     }
